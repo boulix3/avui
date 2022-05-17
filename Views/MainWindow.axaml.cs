4d18d6bd16591cf0036558a88401e37aa8578471
boulix3 <boulix3@gmail.com>
2022-05-17 10:56:44 +0200

    initialize project

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace avui.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
